91084da16969c10ee36f127059b5c1a33b709628
SKolevatov <sergei.kolevatov@nytimes.com>
2018-08-30 06:30:28 -0400

    v3 reCAPTCHA (ii)

--- handleCaptcha.js
+++ handleCaptcha.js
@@ -3,17 +3,19 @@ const tokenJumbotron = document.getElementById("token");
 const tokenContainer = document.getElementById("token-place");
 const googleRespJumbotron = document.getElementById("google");
 const googleRespContainer = document.getElementById("response");
+const secret = '6LcBRm0UAAAAABBCGBY5arCUOdDSCBITuJoHBm5t';
+const siteKey = '6LcBRm0UAAAAAP-ZmkTiqf4Zbd657frckbBHTSgE';
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     grecaptcha.ready(function() {
-        grecaptcha.execute('6LcBRm0UAAAAAP-ZmkTiqf4Zbd657frckbBHTSgE', {action: 'button_clicked'})
+        grecaptcha.execute(siteKey, {action: 'button_clicked'})
             .then(function(token) {
                 tokenJumbotron.classList.remove('d-none');
                 tokenContainer.innerHTML = token;
                 fetch('https://www.google.com/recaptcha/api/siteverify', {
                     method: 'post',
-                    body: `secret=6Lek-2sUAAAAABH9-lM5B5OwOAnK29Xc7tOJd3-H&response=${token}`,
+                    body: `secret=${secret}&response=${token}`,
                     headers: {
                         'Content-Type': 'application/x-www-form-urlencoded'
                     }
